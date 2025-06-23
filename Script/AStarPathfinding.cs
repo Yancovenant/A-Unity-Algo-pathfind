e6231b9c78923deea9857a6f756d80f971790a2e
Yancovenant <tirtamoto@gmail.com>
2025-06-23 12:48:42 +0700

    Update use ReservationTable as respect when finding a path AStarPathfinding.cs
    using custom define AStar Algo

--- Script/AStarPathfinding.cs
+++ Script/AStarPathfinding.cs
@@ -2,12 +2,11 @@
 * Pathfinding.cs
 * Handles the pathfinding system used by PathCoordinator.cs.
 * Using A* algorithm.
+* RESPECT reservation table.
 */
 
 /**
-/* TODO: CHANGE THIS INTO UNITY ENGINE AI PATHFIND
-/* I JUST GET TO KNOW THAT UNITY PATHFIND IS USING A*
-/* FALL BACK AND ADD GENTLE REMINDER
+/ -- (refactored for Cooperative A*)
 /*/
 
 using UnityEngine;
@@ -17,13 +16,29 @@ public class Pathfinding : MonoBehaviour {
     void Awake() {}
 }
 
-/**
-* This public exposed class is used to find the path using A* algorithm.
-* use: AStarPathfinder.FindPath(grid, startPos, targetPos);
-*/
 public static class AStarPathfinder {
+    // A helper class for the A* algo to keep track of nodes in the search space,
+    // which includes time.
+    private class PathNode {
+        public Node node;
+        public int timestep;
+        public int gCost; // Cost from start (eq to time);
+        public int hCost; // Heuristic cost to end
+        public PathNode parent;
 
-    public static List<Node> FindPath(GridManager grid, Vector3 startPos, Vector3 targetPos) {
+        public int fCost { get {return gCost + hCost;}}
+
+        public PathNode(Node node, int timestep, int gCost, int hCost, PathNode parent) {
+            this.node = node;
+            this.timestep = timestep;
+            this.gCost = gCost;
+            this.hCost = hCost;
+            this.parent = parent;
+        }
+    }
+
+    // A* Now will considers time and reservation table send by pathcoordinator.cs;
+    public static List<Node> FindPath(GridManager grid, Vector3 startPos, Vector3 targetPos, string agentId, int startTime, Dictionary<int, Dictionary<Node, string>> reservationTable) {
         /**
         * Alright so to summarize a* algorithm,
         * we will compute it by checking the cost of the node,
@@ -35,12 +50,19 @@ public static class AStarPathfinder {
         Node startNode = grid.NodeFromWorldPoint(startPos);
         Node targetNode = grid.NodeFromWorldPoint(targetPos);
 
-        List<Node> openSet = new List<Node>();
-        HashSet<Node> closedSet = new HashSet<Node>();
-        openSet.Add(startNode);
+        List<PathNode> openSet = new List<PathNode>();
+        HashSet<Node> closedSet = new HashSet<Node>(); // still useful for pruning
+        Dictionary<(Node, int), PathNode> allNodes = new Dictionary<(Node, int), PathNode>();
+
+        PathNode startPathNode = new PathNode(startNode, startTime, 0, GetDistance(startNode, targetNode), null);
+        openSet.Add(startPathNode);
+        allNodes.Add((startNode, startTime), startPathNode);
+
+        int maxTime = 100; //safety break to prevent infinite loops
 
         while (openSet.Count > 0) {
-            Node currentNode = openSet[0];
+            PathNode currentPathNode = openSet[0];
+            //Node currentNode = openSet[0];
             /**
             * First we will check the cost of the node,
             * we could check it like this:
@@ -49,9 +71,9 @@ public static class AStarPathfinder {
             * if so, we will set the currentNode to the node of this iteration,
             */
             for (int i = 1; i < openSet.Count; i++) {
-                if (openSet[i].fCost < currentNode.fCost ||
-                    (openSet[i].fCost == currentNode.fCost && openSet[i].hCost < currentNode.hCost)) {
-                    currentNode = openSet[i];
+                if (openSet[i].fCost < currentPathNode.fCost ||
+                    (openSet[i].fCost == currentPathNode.fCost && openSet[i].hCost < currentPathNode.hCost)) {
+                    currentPathNode = openSet[i];
                 }
             }
             /**
@@ -60,13 +82,21 @@ public static class AStarPathfinder {
             * if the currentNode is the targetNode, it means we have found the path,
             * we will return the path,
             */
-            openSet.Remove(currentNode);
-            closedSet.Add(currentNode);
-            if (currentNode == targetNode) {
-                // Retrace the path
-                List<Node> finalPath = RetracePath(grid, startNode, targetNode);
-                return finalPath;
+            openSet.Remove(currentPathNode);
+
+            if (currentPathNode.node == targetNode) {
+                // Retrace the path if path found.
+                // this is responsible for returning a path and breaking the loop
+                // only if the full path is found.
+                return RetracePath(currentPathNode);
             }
+
+            if (currentPathNode.timestep > maxTime) continue; // safetly call
+
+            // -- we can now get neighbours AND wait in place ---
+            List<Node> neighbours = grid.GetNeighbours(currentPathNode.node);
+            neighbours.Add(currentPathNode.node); // add the current node it self
+            //closedSet.Add(currentNode);
             /**
             * then we check for the neighbour of the currentNode,
             * if the neighbour is not walkable or already in closedSet, skip it.
@@ -76,15 +106,40 @@ public static class AStarPathfinder {
             * we can set the gCost, hCost, and parent of the neighbour,
             * and add it to the openSet,
             */
-            foreach (Node neighbour in grid.GetNeighbours(currentNode)) {
-                if (!neighbour.walkable || closedSet.Contains(neighbour)) continue;
-                int newCostToNeighbour = currentNode.gCost + GetDistance(currentNode, neighbour);
-                if (newCostToNeighbour < neighbour.gCost || !openSet.Contains(neighbour)) {
-                    neighbour.gCost = newCostToNeighbour;
-                    neighbour.hCost = GetDistance(neighbour, targetNode);
-                    neighbour.parent = currentNode;
-                    if (!openSet.Contains(neighbour)) openSet.Add(neighbour);
+            foreach (Node neighbourNode in neighbours) {
+                if (!neighbourNode.walkable) continue;
+                int newTimestep = currentPathNode.timestep + 1;
+
+                // -- Conflict check ---
+                // Is the neighbour node reserved by ANOTHER agent at the future timestep?
+                bool isReserved = false;
+                if (reservationTable.ContainsKey(newTimestep) && reservationTable[newTimestep].ContainsKey(neighbourNode)) {
+                    if(reservationTable[newTimestep][neighbourNode] != agentId) {
+                        isReserved = true;
+                    }
                 }
+                // also check if we are swapping with another agent
+                if (reservationTable.ContainsKey(newTimestep) &&
+                    reservationTable[newTimestep].ContainsKey(currentPathNode) &&
+                    reservationTable.ContainsKey(currentPathNode.timestep) &&
+                    reservationTable[currentPathNode.timestep].ContainsKey(neighbourNode)) {
+                        if (reservationTable[newTimestep][currentPathNode.node] != agentId &&
+                            reservationTable[currentPathNode.timestep][neighbourNode] != agentId) {
+                                isReserved = true;
+                            }
+                    }
+                if (isReserved) continue; // path is blocked by reservation, cannot move here at this time.
+
+                int gCost = currentPathNode.gCost + 1; // Each Step cost 1 timestep;
+                int hCost = GetDistance(neighbourNode, targetNode);
+                var key = (neighbourNode, newTimestep);
+
+                if(allNodes.ContainsKey(key) && allNodes[key].gCost <= gCost) continue;
+                
+                PathNode neighbourPathNode = new PathNode(neighbourNode, newTimestep, gCost, hCost, currentPathNode);
+
+                if(!openSet.Contains(neighbourPathNode)) openSet.Add(neighborPathNode); // could be improve with a PriorityQueue;
+                allNodes[key] = neighbourPathNode;
             }
             /**
             * This will keep going on and on until the openSet is empty,
@@ -106,15 +161,14 @@ public static class AStarPathfinder {
     * and go back to the startNode,
     * and add the node to the path,
     */
-    static List<Node> RetracePath(GridManager grid, Node startNode, Node endNode) {
+    private static List<Node> RetracePath(PathNode endPathNode) {
         List<Node> path = new List<Node>();
-        Node currentNode = endNode;
-        while (currentNode != startNode) {
-            path.Add(currentNode);
-            currentNode = currentNode.parent;
+        Node currentPathNode = endPathNode;
+        while (currentPathNode != null) {
+            path.Add(currentPathNode.node);
+            currentPathNode = currentPathNode.parent;
         }
         path.Reverse();
-        grid.path = path;
         return path;
     }
     /**
@@ -125,9 +179,10 @@ public static class AStarPathfinder {
     * we will return the distance as 14 * dstY + 10 * (dstX - dstY),
     * otherwise we will return the distance as 14 * dstX + 10 * (dstY - dstX),
     */
-    static int GetDistance(Node a, Node b) {
-        int dstX = Mathf.Abs(a.gridX - b.gridX);
-        int dstY = Mathf.Abs(a.gridY - b.gridY);
+    private static int GetDistance(Node a, Node b) {
+        int dstX = Mathf.Abs(a.gridX - b.gridX); //distance x -> absolute ax - bx
+        int dstY = Mathf.Abs(a.gridY - b.gridY); // distance y -> absolute ay - by
+        // if distance x > distance y
         return (dstX > dstY) ? 14 * dstY + 10 * (dstX - dstY) : 14 * dstX + 10 * (dstY - dstX);
     }
 }
